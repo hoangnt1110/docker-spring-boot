9c1bd2e4c2121c9914279f6e1c7b5ffc2cbe4cbf
Admin <admin>
2019-04-08 22:21:50 +0700

    docker complete

--- src/main/java/com/example/howtodoinjava/hellodocker/HelloDockerApplication.java
+++ src/main/java/com/example/howtodoinjava/hellodocker/HelloDockerApplication.java
@@ -1,12 +1,10 @@
 package com.example.howtodoinjava.hellodocker;
 
-import java.util.Date;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloDockerApplication {
@@ -16,13 +14,11 @@ public class HelloDockerApplication {
 	}
 }
 
-@RestController
+@Controller
 class HelloDockerRestController {
 	@RequestMapping("/hello/{name}")
 	public String helloDocker(@PathVariable(value = "name") String name) {
-		String response = "Hello " + name + " Response received on : " + new Date();
-		System.out.println(response);
-		return response;
+		return "index";
 
 	}
 }
